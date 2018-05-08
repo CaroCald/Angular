b40ba9b2630bae689081d71f7cbe6efcd6af6232
CaroCald <carolina.2594@hotmail.com>
2018-05-08 17:08:31 -0500

    pipes text

--- spoilerApp6/src/app/app.component.ts
+++ spoilerApp6/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //etiqueta
+  templateUrl: './app.component.html', //structura
+  styleUrls: ['./app.component.css'] //
 })
 export class AppComponent {
-  title = 'app';
+  nombre= 'Carolina';
+  apellido='Calderon';
+  edad=23;
+  sueldo=477747.33;
+  fechaNacimiento= new Date('1994-02-25');
+  nombreBoton='Cambiar Color';
+  nombreClase ='sa-titulo';
+
+  cambiarClaseDeTitulo(){
+    const nombreClaseEsTitulo=this.nombreClase==='sa-titulo'; //nueva variable con la que representar el if
+    if(nombreClaseEsTitulo){
+      this.nombreClase='sa-subtitulo';
+    }else{
+      this.nombreClase='sa-titulo;'
+    }
+  }
+  aplicarClase(valorInput: string){
+    this.nombreClase=valorInput;
+  }
+
 }
--- spoilerApp6/src/app/app.module.ts
+++ spoilerApp6/src/app/app.module.ts
@@ -7,10 +7,11 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ], //definir todos los componentes a usarse
+  // definir todos los pipes -> transformar los datos
   imports: [
     BrowserModule
-  ],
-  providers: [],
+  ], //definimos todos los modulos
+  providers: [], // definimos los servicios
   bootstrap: [AppComponent] //componente principal
 })
 export class AppModule { }
